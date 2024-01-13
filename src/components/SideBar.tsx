e181ef9563c33d082d11fd922ff00731a578e450
Éverton Carvalho <evertonsnake@gmail.com>
2024-01-13 15:02:30 -0300

    Conversation AI UI

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -65,7 +65,7 @@ export const SideBar = () => {
 	const pathname = usePathname();
 
 	return (
-		<div className="space-y-4 felx py-4 flex-col h-full bg-card-foreground text-foreground">
+		<div className="space-y-4 felx py-4 flex-col h-full bg-card text-foreground">
 			<div className="px-3 py-2 flex-1">
 				<Link href="/dashboard" className="flex items-center pl-3 mb-14">
 					<div className="relative w-8 h-8 mr-4">
