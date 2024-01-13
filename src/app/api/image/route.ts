b7a7ffa97af77e4c38b477977c2884c260aca74b
Éverton Carvalho <evertonsnake@gmail.com>
2024-01-13 20:48:46 -0300

    image generation

---
+++ src/app/api/image/route.ts
@@ -0,0 +1,47 @@
+// New
+import { auth } from '@clerk/nextjs';
+import { NextResponse } from 'next/server';
+import OpenAI from 'openai';
+
+const openai = new OpenAI({
+	apiKey: process.env.OPENAI_API_KEY, // This is also the default, can be omitted
+});
+
+export async function POST(req: Request) {
+	try {
+		const { userId } = auth();
+		const body = await req.json();
+		const { prompt, amount = 1, resolution = '512x512' } = body;
+
+		if (!userId) {
+			return new NextResponse('Unauthorized', { status: 401 });
+		}
+
+		if (!openai.apiKey) {
+			return new NextResponse('Openai API Key not Configured', { status: 500 });
+		}
+
+		if (!prompt) {
+			return new NextResponse('Prompt is required', { status: 400 });
+		}
+
+		if (!amount) {
+			return new NextResponse('Amount is required', { status: 400 });
+		}
+
+		if (!resolution) {
+			return new NextResponse('Resolution is required', { status: 400 });
+		}
+
+		const response = await openai.images.generate({
+			prompt: prompt,
+			n: parseInt(amount, 10),
+			size: resolution,
+		});
+
+		return NextResponse.json(response.data);
+	} catch (error) {
+		console.log('[IMAGE_ERROR', error);
+		return new NextResponse('Internal error', { status: 500 });
+	}
+}
